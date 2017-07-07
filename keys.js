ceb3c84b884e918ada5a3448fb7f86823125a07e
jcostopulos <costopulos@gmail.com>
2017-07-06 17:15:46 -0700

    file structure in place and twitter keys added

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<sbdI5fCqYu4U3VnmYdkGDqBR8>',
+  consumer_secret: '<liRZGJ6c3ijok86rgFuqe7Lh9wDBhAm1PqbMUICyY2XY1kI4ma>',
+  access_token_key: '<883040181460254720-4TdJ2wxtRMbGdbuLv3TtcAnk02DNGvQ>',
+  access_token_secret: '<y3SBVi61y8GeqxZrQMgvnyB0u7CA8NLEqoJUtAmUmVV30>',
+};
